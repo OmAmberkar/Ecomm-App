cb832bc837679964723e6561c051e0e01ad5703a
unknown <omamberkar08@gmail.com>
2026-02-18 13:16:00 +0530

    console response changed in app.ts file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -33,7 +33,6 @@ app.use((err: Error, _req: express.Request, res: express.Response, _next: expres
 app.listen(PORT, () => {
 
   console.log(`Server running on : http://localhost:${PORT}`);
-  console.log(`Check products on : http://localhost:${PORT}/api/products`);
 });
 
 export default app;
